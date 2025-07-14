3b5d84fa7387bd73a5501acdaabc97324b69bfd4
aphatab <aftabmalick64@.in>
2025-07-14 21:42:14 +0530

    calculator in java

--- src/Conditions.java
+++ src/Conditions.java
@@ -5,33 +5,48 @@ public class Conditions {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int button = sc.nextInt();
 
-switch (button) {
-    case 1 : System.out.println("january");
-    break;
-    case  2 : System.out.println("febraury");
-    break;
-    case 3 : System.out.println("march");
-    break;
-    case 4 : System.out.println("april");
-        break;
-    case 5 : System.out.println("may");
-    break;
-    case 6 : System.out.println( "june");
-    break;
-    case 7 : System.out.println( "july");
-    break;
-    case  8 : System.out.println("august");
-        break;
-    case  9 : System.out.println("september");
-    break;
-    case  10 : System.out.println("october");
-        break;
-    case  11 : System.out.println("november");
-    break;
-    case 12 : System.out.println("december");
-    break;
+        System.out.println("enter first number(a):");
+        int a = sc.nextInt();
+
+        System.out.println("enter first number(b):");
+        int b = sc.nextInt();
+
+        System.out.println("Choose operation:");
+        System.out.println("1 : + (Addition)");
+        System.out.println("2 : - (Subtraction)");
+        System.out.println("3 : * (Multiplication)");
+        System.out.println("4 : / (Division)");
+        System.out.println("5 : % (Modulo)");
+
+        int op = sc.nextInt();
+
+        switch (op) {
+            case 1:
+                System.out.println("Result: " + (a + b));
+                break;
+            case 2:
+                System.out.println("Result: " + (a - b));
+                break;
+            case 3:
+                System.out.println("Result: " + (a * b));
+                break;
+            case 4:
+                if (b != 0) {
+                    System.out.println("Result: " + (a / b));
+                } else {
+                    System.out.println("Error: Cannot divide by zero");
+                }
+                break;
+            case 5:
+                if (b != 0) {
+                    System.out.println("Result: " + (a % b));
+                } else {
+                    System.out.println("Error: Cannot perform modulo by zero");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation selected");
         }
     }
 }
