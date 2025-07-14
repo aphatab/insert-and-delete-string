161dac773748c5c80d94d6a8942514ac6a4a4beb
aphatab <aftabmalick64@.in>
2025-07-14 14:27:53 +0530

    else if statment

--- src/Conditions.java
+++ src/Conditions.java
@@ -2,14 +2,19 @@ import java.util.*;
 
 public class Conditions {
 
-public static void main (String[] args){
+public static void main (String[] args) {
     Scanner sc = new Scanner(System.in);
-    int x = sc.nextInt();
+    int  a = sc.nextInt();
+    int b = sc.nextInt();
 
-    if(x % 2 == 0) {
-        System.out.println("Even");
-    }else {
-        System.out.println("odd");
+    if (a == b){
+        System.out.println("equal");
+    } else if (a > b) {
+        System.out.println("a is greater");
+    }
+    else  {
+        System.out.println("a is lesser");
+
     }
 
 }
