6f10bd485fc998f87700e54a476336d5fd8233ad
aphatab <aftabmalick64@.in>
2025-07-14 15:55:48 +0530

    greeting

--- src/Conditions.java
+++ src/Conditions.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 public class Conditions {
 
-public static void main (String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int  a = sc.nextInt();
-    int b = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
 
-    if (a == b){
-        System.out.println("equal");
-    } else if (a > b) {
-        System.out.println("a is greater");
+        if(button == 1) {
+            System.out.println("hello");
+        }
+        else if(button == 2) {
+            System.out.println("namste");
+        }
+        else if(button == 3) {
+            System.out.println("bonjor");
+        } else {
+            System.out.println("invalid button");
+        }
     }
-    else  {
-        System.out.println("a is lesser");
-
-    }
-
-}
 }
