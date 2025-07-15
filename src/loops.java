c4a4397c6e790f44513a65f5335493949c23d185
aphatab <aftabmalick64@.in>
2025-07-15 16:29:52 +0530

    do while loop

--- src/loops.java
+++ src/loops.java
@@ -1,10 +1,10 @@
 public class loops {
-
-    public static void main(String agrs[]){
-         int i = 0 ;
-                while (i < 11) {
-                    System.out.println(i);
-                    i++;
-                }
+    public static void main (String agrs []){
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i < 11);
     }
 }
