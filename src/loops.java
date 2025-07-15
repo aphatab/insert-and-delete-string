389931f759d6802998e3753d6b3c229f19bb04d7
aphatab <aftabmalick64@.in>
2025-07-15 17:58:54 +0530

    nested loop

--- src/loops.java
+++ src/loops.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 public class loops {
     public static void main (String agrs []){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = 4 ;
+        int m = 5 ;
 
-
-        int sum =  0 ;
-        for (int i =0; i<=n; i++){
-            sum = sum + i ;
+        for (int i =1; i <=n; i++){
+         for (int j =1 ; j <=m; j++){
+             System.out.print("*");
+         }
+            System.out.println();
         }
-        System.out.println(sum);
     }
 }
