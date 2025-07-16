89db1df91d0143bee11ff0487d78d9b0a798f1bb
aphatab <aftabmalick64@.in>
2025-07-16 20:21:13 +0530

    Half peramet

--- src/loops.java
+++ src/loops.java
@@ -2,23 +2,19 @@ import java.util.*;
 
 public class loops {
     public static void main (String agrs []){
-        int n = 4 ;
-        int m = 5 ;
+        int n = 4;
+
 
         //outer loop
-        for (int i=1; i<=n; i++){
-            //iner loop
-             for (int j=1; j<=m; j++){
-                 //cell
+        for (int i = 1; i <= n; i++){
+            //inner loop
+             for (int j = 1; j <= i; j++){
 
-                 if (i == 1 || j == 1  || i == n || j == m){
-                     System.out.print("*");
-                 }else {
-                     System.out.print(" ");
-                 }
-
+                 System.out.print("*");
              }
+
             System.out.println();
+
         }
 
     }
