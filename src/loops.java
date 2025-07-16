10b76b50d0f3008c4a3d49adfdfa659df7563e73
aphatab <aftabmalick64@.in>
2025-07-16 20:52:17 +0530

    invert half pyramid

--- src/loops.java
+++ src/loops.java
@@ -6,9 +6,9 @@ public class loops {
 
 
         //outer loop
-        for (int i = 1; i <= n; i++){
+        for (int i=n; i>= 1; i--){
             //inner loop
-             for (int j = 1; j <= i; j++){
+             for (int j= 1; j<= i; j++){
 
                  System.out.print("*");
              }
