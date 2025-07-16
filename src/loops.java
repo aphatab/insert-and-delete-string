e16d937f3d29653902e09fd1092fef62e48b8f7f
aphatab <aftabmalick64@.in>
2025-07-16 17:24:55 +0530

    hollow rectangle

--- src/loops.java
+++ src/loops.java
@@ -5,11 +5,21 @@ public class loops {
         int n = 4 ;
         int m = 5 ;
 
-        for (int i =1; i <=n; i++){
-         for (int j =1 ; j <=m; j++){
-             System.out.print("*");
-         }
+        //outer loop
+        for (int i=1; i<=n; i++){
+            //iner loop
+             for (int j=1; j<=m; j++){
+                 //cell
+
+                 if (i == 1 || j == 1  || i == n || j == m){
+                     System.out.print("*");
+                 }else {
+                     System.out.print(" ");
+                 }
+
+             }
             System.out.println();
         }
+
     }
 }
