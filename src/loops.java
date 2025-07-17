2d5b6fddb7a5b97c7a99cd9469ec194dc230c7a6
aphatab <aftabmalick64@.in>
2025-07-17 12:37:59 +0530

    pryramid rotated by 180 deg

--- src/loops.java
+++ src/loops.java
@@ -1,18 +1,17 @@
 import java.util.*;
 
 public class loops {
-    public static void main (String agrs []){
+    public static void main(String args[]) {
         int n = 4;
 
+        for(int i=1 ; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
 
-        //outer loop
-        for (int i=n; i>= 1; i--){
-            //inner loop
-             for (int j= 1; j<= i; j++){
-
-                 System.out.print("*");
-             }
-
+            }
             System.out.println();
 
         }
