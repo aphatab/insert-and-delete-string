1e9616fc2beba915e3969a91f396b9ef747d7f33
aphatab <aftabmalick64@.in>
2025-07-17 18:10:33 +0530

    floyd's triangle

--- src/loops.java
+++ src/loops.java
@@ -3,12 +3,14 @@ import java.util.*;
 public class loops {
     public static void main(String args[]) {
         int n = 5;
+        int number = 1;
 //outer loop
-        for(int i=1 ; i<=n; i++) {
+        for(int i=1; i<=n; i++) {
 
             //iner loop
-            for(int j=1; j<=n-i+1; j++) {
-                System.out.print(j);
+            for(int j=1; j<=i; j++) {
+                System.out.print(number);
+                number++;
 
             }
             System.out.println();
