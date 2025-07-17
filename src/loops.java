03041de9c85b88a90239ee7bc48a0709041cf96a
aphatab <aftabmalick64@.in>
2025-07-17 13:01:59 +0530

    half pyramid witg number

--- src/loops.java
+++ src/loops.java
@@ -2,14 +2,13 @@ import java.util.*;
 
 public class loops {
     public static void main(String args[]) {
-        int n = 4;
-
+        int n = 5;
+//outer loop
         for(int i=1 ; i<=n; i++) {
-            for(int j=1; j<=n-i; j++) {
-                System.out.print(" ");
-            }
-            for(int j=1; j<=i; j++){
-                System.out.print("*");
+
+            //iner loop
+            for(int j=1; j<=i; j++) {
+                System.out.print(j+" ");
 
             }
             System.out.println();
