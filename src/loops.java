4c52b26b7fae900e4d6ee308e47e5e4ba3a1012d
aphatab <aftabmalick64@.in>
2025-07-18 16:50:16 +0530

    butterfly pattern

--- src/loops.java
+++ src/loops.java
@@ -1,23 +1,42 @@
-
+import java.util.*;
 
 public class loops {
-    public static void main(String args[]) {
-        int n = 5;
+    public static void main (String args[]){
+      int n = 5;
 
-//outer loop
-        for (int i = 1; i <= n; i++) {
-
-            //iner loop
-            for (int j = 1; j <= i; j++) {
-                int sum = i + j;
-                if (sum % 2 == 0) {//even
-                    System.out.print("1");
-                } else {//odd
-                    System.out.print("0");
-                }
+      //- uppper half
+        for(int i=1; i<=n; i++){
+            //1st part
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            //spaces
+            int spaces = 2 *(n-i);
+            for (int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            //2nd part
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
             }
             System.out.println();
-
+        }
+        //lower half
+        for(int i=n; i>=1; i--){
+            //1st part
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            //spaces
+            int spaces = 2*(n-i);
+            for (int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            //2nd part
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
-   }
+}
