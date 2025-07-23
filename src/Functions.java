4fa64e602671dfc1b6a05f9a0de5ebed6fd03f95
aphatab <aftabmalick64@.in>
2025-07-23 23:56:17 +0530

    name in a function

---
+++ src/Functions.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Functions {
+    public static void PrintMyMame(String name){
+        System.out.println(name);
+        return;
+    }
+
+    public static void main (String args []) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        PrintMyMame(name); //call kiya function ko
+
+    }
+}
