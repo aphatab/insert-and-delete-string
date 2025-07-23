ee1b1f1ff3ac967cb561ac15de00fd2f1922b447
aphatab <aftabmalick64@.in>
2025-07-23 23:21:33 +0530

    diamond pattern

--- src/loops.java
+++ src/loops.java
@@ -1,27 +1,31 @@
 import java.util.*;
 
-import java.util.*;
-
-
-public class loops{
+public class loops {
     public static void main(String args[]) {
         int n = 5;
-        for(int i=1; i<=n; i++) {
-            //spaces
-            for(int j=1; j<=n-i; j++) {
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-
-
-            //first part
-            for(int j=i; j>=1; j--) {
-                System.out.print(j);
+            // stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
             }
+            System.out.println();
+        }
 
-
-            //second part
-            for(int j=2; j<=i; j++) {
-                System.out.print(j);
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
