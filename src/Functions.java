23e264575c86b2a02fa707076107eb49c791f4b2
aphatab <aftabmalick64@.in>
2025-07-24 13:17:38 +0530

    2 number sum function

--- src/Functions.java
+++ src/Functions.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
 public class Functions {
-    public static void PrintMyMame(String name){
-        System.out.println(name);
-        return;
+    public static int calculateSum(int a, int b){
+        int sum = a + b;
+        return sum;
     }
 
     public static void main (String args []) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        PrintMyMame(name); //call kiya function ko
+        int sum = calculateSum(a,b);
+        System.out.println("Sum of 2 number is : "+ sum);
+
+
 
     }
 }
