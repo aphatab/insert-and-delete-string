fcf6656046f56228942723dda9d893c8a40c7905
aphatab <aftabmalick64@.in>
2025-07-26 12:18:37 +0530

    twodarrays

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,14 +1,42 @@
 import java.util.*;
 
+
 public class Arrays {
-    public static void main(String args[]){
-        int[] marks = new int[3];
-        marks[0] = 97; //phy
-        marks[1] = 98; //chem
-        marks[2] = 95;//eng
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
-        System.out.println(marks[2]);
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+
+        int[][] numbers = new int[rows][cols];
+
+
+        //input
+        //rows
+        for(int i=0; i<rows; i++) {
+            //columns
+            for(int j=0; j<cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+
+        int x = sc.nextInt();
+
+
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                //compare with x
+                if(numbers[i][j] == x) {
+                    System.out.println("x found at location (" + i + ", " + j + ")");
+                }
+            }
+        }
+
+
+
+
 
     }
 }
+
